6fed951df1866ae335a1f1daa57c1e777034950f
jyotipadhilanet <35651355+jyotipadhilanet@users.noreply.github.com>
2018-03-09 17:49:43 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+var express=require('express')
+var expGraphQL=require('express-graphql')
+var schema=require('./schema/schema')
+var app=express()
+app.use('/graphql',expGraphQL({
+    schema,
+    graphiql:true
+}))
+app.listen(4000,()=>{
+   console.log("server connected on port 4000");
+})
